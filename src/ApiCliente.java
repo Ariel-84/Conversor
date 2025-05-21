1bb6299553cd7738f6e18cfb86acf4a8bbd39e29
ariel-84 <ariel.caferri@hotmail.com>
2025-05-21 15:30:04 -0300

    solicitud HTTP, HttpClient, HttpRequest y HttpResponse

--- src/ApiCliente.java
+++ src/ApiCliente.java
@@ -1,2 +1,26 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ApiCliente {
+    private final String API_KEY = "d6ab6dd8a714614b72466e73";
+
+    public String obtenerTasas(String base) {
+        String url = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/" + base;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return response.body();
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException("Error al obtener tasas de cambio", e);
+        }
+    }
+
 }
--- src/Conversor.java
+++ src/Conversor.java
@@ -1,3 +1,11 @@
 public class Conversor {
 
+    public static void main(String[] args) {
+        ApiCliente api = new ApiCliente();
+        String json = api.obtenerTasas("USD"); // Base USD
+
+        System.out.println("Respuesta de la API:");
+        System.out.println(json);
+    }
+
 }
