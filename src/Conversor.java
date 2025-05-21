8e3e2cc28722f77c11d7a7e272e2fc2467eec6d7
ariel-84 <ariel.caferri@hotmail.com>
2025-05-21 16:05:05 -0300

    Análisis del JSON con Gson

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,13 +1,21 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
 public class Conversor {
     public static void main(String[] args) {
         ApiCliente api = new ApiCliente();
         String json = api.obtenerTasas("USD");
 
         if (json != null) {
-            System.out.println("Respuesta JSON de la API:");
-            System.out.println(json);
-        } else {
-            System.out.println("No se pudo obtener la respuesta.");
+            Gson gson = new Gson();
+
+            // Mostramos solo la tasa para ARS (por ejemplo)
+            JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
+            JsonObject tasas = jsonObject.getAsJsonObject("conversion_rates");
+
+            double tasaARS = tasas.get("ARS").getAsDouble();
+
+            System.out.println("Tasa USD => ARS: " + tasaARS);
         }
     }
 }
